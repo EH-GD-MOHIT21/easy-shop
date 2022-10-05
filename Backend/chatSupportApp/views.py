70ac6319758501e58cc9dfd0d19c1c0a1de31b3c
hks74123 <hemant91852@gmail.com>
2022-10-05 15:11:44 +0530

    added_views for rendering admin chat and updating queue

--- Backend/chatSupportApp/views.py
+++ Backend/chatSupportApp/views.py
@@ -30,3 +30,25 @@ def create_queue(user_obj,grp_name):
     new_queue.save()
     queue_length = new_queue.id-Queue.objects.first().id
     return queue_length
+
+def get_queue(queue_obj):
+    user_obj = queue_obj.user
+    user_o = Queue.objects.get(user= user_obj)
+    queue_length = user_o.id-Queue.objects.first().id
+    grp_name = user_o.group_name
+    dict_info ={
+        'queue_length':queue_length,
+        'grp_name':grp_name,
+    }
+    return dict_info
+
+def RenderAdminChat(request):
+    if request.user.is_authenticated:
+        if(request.user.staff):
+            try:
+                all_queues = Queue.objects.first()
+                id_queue = all_queues.group_name
+                return render(request, 'admin_chatbox.html',{'queues':all_queues,'queue_id':id_queue}) 
+            except:
+                return render(request, 'info.html',{'message':'No request pending for chat support'})
+    return redirect('/') 
