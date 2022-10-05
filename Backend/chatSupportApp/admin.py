b623f2ae457f3d94c8a4cbbf92421d2db71d7dd5
hks74123 <hemant91852@gmail.com>
2022-10-05 14:48:02 +0530

    added_models and admin for chatsupport

--- Backend/chatSupportApp/admin.py
+++ Backend/chatSupportApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from chatSupportApp.models import Chat, Queue
 # Register your models here.
+
+
+class CustomChat(admin.ModelAdmin):
+    list_display = ["user","group","timestamp"]
+    search_fields = ["user__username","user__email","group","timestamp"]
+
+
+class CustomQueue(admin.ModelAdmin):
+    list_display = ["user","group_name","joined_at"]
+    search_fields = ["user__username","user__email","group_name","joined_at"]
+
+
+admin.site.register(Chat,CustomChat)
+admin.site.register(Queue,CustomQueue)
