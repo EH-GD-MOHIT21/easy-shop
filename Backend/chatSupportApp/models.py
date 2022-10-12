f5823a7f53f7c515e28e969a6cc0dc51cb78b932
PinuBhai <115623167+PinuBhai@users.noreply.github.com>
2022-10-12 15:14:53 +0530

    PEP8 Formatter

--- Backend/chatSupportApp/models.py
+++ Backend/chatSupportApp/models.py
@@ -4,26 +4,28 @@ from dukanAuthApp.models import User
 
 # Create your models here.
 
+
 class Chat(models.Model):
-    user=models.ForeignKey(
+    user = models.ForeignKey(
         User,
         on_delete=models.CASCADE
-    ) 
+    )
 
-    group=models.CharField(
+    group = models.CharField(
         max_length=20
     )
 
-    message=models.CharField(
+    message = models.CharField(
         max_length=200
     )
 
-    timestamp=models.DateTimeField(
+    timestamp = models.DateTimeField(
         null=True
     )
 
     def __str__(self):
         return(self.message)
+
 
 class Queue(models.Model):
     user = models.ForeignKey(
@@ -33,10 +35,9 @@ class Queue(models.Model):
 
     group_name = models.CharField(
         max_length=25,
-        null=True,blank=True 
+        null=True, blank=True
     )
 
     joined_at = models.DateTimeField(
         null=True
     )
-    
--- Backend/chatSupportApp/sockets/consumers.py
+++ Backend/chatSupportApp/sockets/consumers.py
@@ -10,10 +10,10 @@ from chatsupportAPP.models import Chat, Queue
 from chatsupportAPP import views
 
 
-class MyASyncConsumer(AsyncWebsocketConsumer): 
+class MyASyncConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.group_name=self.scope['url_route']['kwargs']['groupname']
-        self.pid=self.scope['url_route']['kwargs']['pid']
+        self.group_name = self.scope['url_route']['kwargs']['groupname']
+        self.pid = self.scope['url_route']['kwargs']['pid']
         user = self.scope['user']
         if user.is_anonymous:
             await self.close()
@@ -28,58 +28,59 @@ class MyASyncConsumer(AsyncWebsocketConsumer):
             if(user.staff):
                 self.group_name = self.group_name
                 await self.channel_layer.group_send(
-                self.group_name,
-                {
-                'type':'chat.message',
-                'message': f"Hii agent is here how can i help you!!",
-                'username':user.username,
-                'updation':'no'
-                }
+                    self.group_name,
+                    {
+                        'type': 'chat.message',
+                        'message': f"Hii agent is here how can i help you!!",
+                        'username': user.username,
+                        'updation': 'no'
+                    }
                 )
-                queue_obj = await sync_to_async(Queue.objects.get)(group_name = self.group_name)
+                queue_obj = await sync_to_async(Queue.objects.get)(group_name=self.group_name)
                 await sync_to_async(views.remove_queue)(queue_obj)
                 for i in await self.get_all_queues():
-                    queue_info = await sync_to_async(views.get_queue)(i) 
+                    queue_info = await sync_to_async(views.get_queue)(i)
                     await self.channel_layer.group_send(
-                    queue_info['grp_name'],
-                    {
-                    'type':'chat3.message',
-                    'message': f"{queue_info['queue_length']}",
-                    })
+                        queue_info['grp_name'],
+                        {
+                            'type': 'chat3.message',
+                            'message': f"{queue_info['queue_length']}",
+                        })
             else:
-                queue_len = await sync_to_async(views.create_queue)(user,self.group_name)
+                queue_len = await sync_to_async(views.create_queue)(user, self.group_name)
                 await self.channel_layer.group_send(
-                self.group_name,
-                {
-                'type':'chat2.message',
-                'message': f"{queue_len}",
-                'username':user.username,
-                }
+                    self.group_name,
+                    {
+                        'type': 'chat2.message',
+                        'message': f"{queue_len}",
+                        'username': user.username,
+                    }
                 )
-    async def chat2_message(self,event):
+
+    async def chat2_message(self, event):
         await self.send(text_data=json.dumps({
-            'msg':event['message'],
-            'status':'no',
-            'updation':'no',
-            'username':event['username'],
+            'msg': event['message'],
+            'status': 'no',
+            'updation': 'no',
+            'username': event['username'],
         }))
 
-    async def chat_message(self,event):
+    async def chat_message(self, event):
         await self.send(text_data=json.dumps({
-            'msg':event['message'],
-            'status':'yes',
-            'updation':'no',
-            'username':event['username'],
+            'msg': event['message'],
+            'status': 'yes',
+            'updation': 'no',
+            'username': event['username'],
         }))
-    
-    async def receive(self,text_data=None,bytes_data=None):
-        print('message recived succesfully...',text_data)
-        data=json.loads(text_data)
-        message=data['msg']
+
+    async def receive(self, text_data=None, bytes_data=None):
+        print('message recived succesfully...', text_data)
+        data = json.loads(text_data)
+        message = data['msg']
         user1 = self.scope["user"]
-        chat=Chat(
+        chat = Chat(
             user=user1,
-            group=self.group_name  ,
+            group=self.group_name,
             message=data['msg'],
             timestamp=datetime.datetime.now(timezone.utc)
         )
@@ -87,55 +88,59 @@ class MyASyncConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(
             self.group_name,
             {
-            'type':'chat1.message',
-            'message': message,
-            'username': user1.username,
+                'type': 'chat1.message',
+                'message': message,
+                'username': user1.username,
             }
         )
-    async def chat1_message(self,event):
+
+    async def chat1_message(self, event):
         await self.send(text_data=json.dumps({
-            'msg':event['message'],
-            'status':'yes',
-            'updation':'no',
-            'username':event['username'],
+            'msg': event['message'],
+            'status': 'yes',
+            'updation': 'no',
+            'username': event['username'],
         }))
+
     @sync_to_async
     def get_all_queues(self):
         return list(Queue.objects.all())
-         
-    async def disconnect(self,event):
-        print('websocket disconnected...',event)
+
+    async def disconnect(self, event):
+        print('websocket disconnected...', event)
         user = self.scope['user']
         if user.is_anonymous:
             await self.close()
-        elif(user.staff == False):  
+        elif(user.staff == False):
             await self.channel_layer.group_send(
                 self.group_name,
                 {
-                    'type':'chat5.message',
+                    'type': 'chat5.message',
                     'message': "User left Please refresh Page to Chat to new user",
-                    'username': user.username      
+                    'username': user.username
                 }
             )
             await sync_to_async(views.remove_queue1)(user)
             for i in await self.get_all_queues():
-                queue_info = await sync_to_async(views.get_queue)(i) 
+                queue_info = await sync_to_async(views.get_queue)(i)
                 await self.channel_layer.group_send(
-                queue_info['grp_name'],
-                {
-                'type':'chat3.message',
-                'message': f"{queue_info['queue_length']}",
-                })
-    async def chat3_message(self,event):
+                    queue_info['grp_name'],
+                    {
+                        'type': 'chat3.message',
+                        'message': f"{queue_info['queue_length']}",
+                    })
+
+    async def chat3_message(self, event):
         await self.send(text_data=json.dumps({
-            'msg':event['message'],
-            'status':'no',
-            'updation':'yes',
+            'msg': event['message'],
+            'status': 'no',
+            'updation': 'yes',
         }))
-    async def chat5_message(self,event):
+
+    async def chat5_message(self, event):
         await self.send(text_data=json.dumps({
-            'msg':event['message'],
-            'username':event['username'],
+            'msg': event['message'],
+            'username': event['username'],
         }))
         self.channel_layer.group_discard(
             self.group_name,
