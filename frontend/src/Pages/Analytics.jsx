9b3c5f3418712af85dfa26a548a1a69b723311b9
hks74123 <hemant91852@gmail.com>
2022-11-27 02:45:51 +0530

    new

--- frontend/src/Pages/Analytics.jsx
+++ frontend/src/Pages/Analytics.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
-
+import "./Analytics.css"
 export default function Analytics() {
   return (
-    <div>Analytics</div>
+    <div className='Analytics'>
+      <div className='Analytics_Header'>
+        Analytics
+      </div>
+      <div className='Analytics_Body'>
+      <div className='order_per_day'>
+        <p className='order_per_day_text'>AVERAGE ORDERS PER DAY</p>
+        <p className='order_per_day_number'>8000</p>
+      </div>
+      <div className='order_value'>
+        <p className='order_value_text'>AVERAGE ORDER VALUE</p>
+        <p className='order_value_number'>₹ 8000</p>
+      </div>
+      <div className='sales_per_day'>
+        <p className='sales_per_day_text'>AVERAGE SALES PER DAY</p>
+        <p className='sales_per_day_number'>₹ 8000</p>
+      </div>
+      <div className='return_coutmer'>
+        <p className='return_coutmer_text'>RETURNING CUSTOMERS</p>
+        <p className='return_coutmer_number'>8000 %</p>
+      </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
-
+import "./Home.css"
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { IconButton } from '@mui/material';
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='dukaan_Home'>
+      <div className='share_store_link'>
+        <p className='share_store_link_text'>Share store link</p>
+        <p className='share_store_link_text2'>Customers can visit the following link and place their orders.</p>
+        <p className='dukan_Link'>mydukaan.io/pinu27</p>
+        <div className='share_dukaan'>
+          <p>Share via</p>
+          <div>
+          <IconButton>
+            <WhatsAppIcon />
+          </IconButton>
+          <IconButton>
+            <FacebookIcon />
+          </IconButton>
+          <IconButton>
+            <TwitterIcon />
+          </IconButton>
+        </div>
+        </div>
+      </div>
+      <div className='store_total_sales'>store_total_sales</div>
+      <div className='store_views'>
+        <div>store_views</div>
+        
+      </div>
+    </div>
   )
 }
