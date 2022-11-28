afe2ba9f95fa28fe296aaf31ee5cfa61693a475d
Mohit Satija <experimentallyf@gmail.com>
2022-11-28 19:14:05 +0530

    razorpay

--- Backend/paymentsApp/models.py
+++ Backend/paymentsApp/models.py
@@ -2,18 +2,17 @@ from django.db import models
 from dukanAuthApp.models import User
 from datetime import datetime
 from django.utils import timezone
+from mainApp.models import Cart,SubCart
 
 # Create your models here.
 
 class Order(models.Model):
     order_id = models.SlugField()
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    address = models.TextField()
-    email = models.EmailField()
-    phone_no = models.CharField(max_length=12)
     amount = models.FloatField(default=0)
     order_at = models.DateTimeField()
     order_status = models.CharField(max_length=10)
+    order_items = models.ManyToManyField(SubCart)
 
 
     def save(self,*args,**kwargs):
